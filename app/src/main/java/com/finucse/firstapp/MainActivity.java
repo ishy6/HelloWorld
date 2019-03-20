dee15ffff632255b5111ce1d6c05c9ac713cc2a3
ishy6 <1250968374@qq.com>
2019-03-20 20:56:31 +0800

    update

--- app/src/main/java/com/finucse/firstapp/MainActivity.java
+++ app/src/main/java/com/finucse/firstapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.finucse.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity","onstart");
+        // The activity is about to become visible.
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity","onresume");
+        // The activity has become visible (it is now "resumed").
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity","onpause");
+        // Another activity is taking focus (this activity is about to be "paused").
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity","onstop");
+        // The activity is no longer visible (it is now "stopped")
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity","ondestroy");
+        // The activity is about to be destroyed.
     }
 }
